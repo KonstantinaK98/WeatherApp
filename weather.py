e1b463520014e9afe9de38196d028021922114eb
KonstantinaK98 <konakara98@gmail.com>
2020-09-23 17:07:59 +0300

    Initialization of app

---
+++ weather.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Weather App")
+root.geometry("400x400")
+root.iconbitmap("images/iconfinder_Weather_Weather_Forecast_Lightning_Storm_Energy_3859139.ico")
+
+
+
+
+root.mainloop()
